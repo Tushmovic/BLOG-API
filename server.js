afae4e505f773b70066cc5218f412cc42c5115e5
Tushmovic <alayabrhm30@gmail.com>
2025-10-01 12:13:29 +0100

    Moved app.js into root folder too

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-dotenv.config();
+require('dotenv').config();
 
 const app = require('./app');
 
